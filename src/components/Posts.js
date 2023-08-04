87b05d44001f5103b84da7214a807fe313d8d4ff
Danny Sun <sunnyhust2005@qq.com>
2023-08-04 22:26:18 +0800

    Fetches posts from backend

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,8 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Spinner } from "react-bootstrap";
+
+const BASE_API_URL = process.env.REACT_APP_BASE_API_URL;
 
 export default function Posts() {
   const [posts, setPosts] = useState();
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(BASE_API_URL + '/api/feed');
+      if (response.ok) {
+        const results = await response.json();
+        setPosts(results.data);
+      } else {
+        setPosts(null);
+      }
+    })();
+  }, []);
 
   return (
     <>
